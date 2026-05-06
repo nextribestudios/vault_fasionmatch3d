7cbf07dc1fbc0959115ca5581aa1d1d50ccbb368
Surya Vamsi K K <kksvamsi@gmail.com>
2026-05-07 05:20:38 +0530

    Added updated firebase tool.

--- Assets/NexTribe/Nextribe_Setup/Editor/FirebaseDownloader.cs
+++ Assets/NexTribe/Nextribe_Setup/Editor/FirebaseDownloader.cs
@@ -16,29 +16,61 @@ namespace Nextribe
 		private const string ZipPath = "Temp/firebase_sdk.zip";
 		private const string ExtractPath = "Temp/firebase_sdk";
 
+		//		private static void AddScriptingDefine(string define)
+		//		{
+		//			var target = EditorUserBuildSettings.selectedBuildTargetGroup;
+
+		//#if UNITY_ANDROID
+		//			string defines = PlayerSettings.GetScriptingDefineSymbols(UnityEditor.Build.NamedBuildTarget.Android);
+		//#endif
+		//#if UNITY_IOS
+		//        string defines = PlayerSettings.GetScriptingDefineSymbols(UnityEditor.Build.NamedBuildTarget.Android);
+		//#endif
+
+		//			if (!defines.Contains(define))
+		//			{
+		//				defines = string.IsNullOrEmpty(defines)
+		//					? define
+		//					: defines + ";" + define;
+		//#if UNITY_ANDROID
+		//				PlayerSettings.SetScriptingDefineSymbols(UnityEditor.Build.NamedBuildTarget.Android, defines);
+		//#endif
+		//#if UNITY_IOS
+
+		//			PlayerSettings.SetScriptingDefineSymbols(UnityEditor.Build.NamedBuildTarget.Android, defines);
+		//#endif
+		//				Debug.Log("Added scripting define: " + define);
+		//			}
+		//			else
+		//			{
+		//				Debug.Log("Scripting define already present: " + define);
+		//			}
+		//		}
+
+		private static string[] GetDefines(UnityEditor.Build.NamedBuildTarget target)
+		{
+			var defines = PlayerSettings.GetScriptingDefineSymbols(target);
+			return string.IsNullOrEmpty(defines)
+				? new string[0]
+				: defines.Split(';');
+		}
+
+		private static void SetDefines(UnityEditor.Build.NamedBuildTarget target, HashSet<string> set)
+		{
+			string result = string.Join(";", set);
+			PlayerSettings.SetScriptingDefineSymbols(target, result);
+		}
+
 		private static void AddScriptingDefine(string define)
 		{
-			var target = EditorUserBuildSettings.selectedBuildTargetGroup;
+			var targetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
+			var target = UnityEditor.Build.NamedBuildTarget.FromBuildTargetGroup(targetGroup);
 
-#if UNITY_ANDROID
-			string defines = PlayerSettings.GetScriptingDefineSymbols(UnityEditor.Build.NamedBuildTarget.Android);
-#endif
-#if UNITY_IOS
-        string defines = PlayerSettings.GetScriptingDefineSymbols(UnityEditor.Build.NamedBuildTarget.Android);
-#endif
+			var defineSet = new HashSet<string>(GetDefines(target));
 
-			if (!defines.Contains(define))
+			if (defineSet.Add(define)) // only adds if not present
 			{
-				defines = string.IsNullOrEmpty(defines)
-					? define
-					: defines + ";" + define;
-#if UNITY_ANDROID
-				PlayerSettings.SetScriptingDefineSymbols(UnityEditor.Build.NamedBuildTarget.Android, defines);
-#endif
-#if UNITY_IOS
-
-			PlayerSettings.SetScriptingDefineSymbols(UnityEditor.Build.NamedBuildTarget.Android, defines);
-#endif
+				SetDefines(target, defineSet);
 				Debug.Log("Added scripting define: " + define);
 			}
 			else
@@ -47,10 +79,120 @@ namespace Nextribe
 			}
 		}
 
+		private static void RemoveScriptingDefine(string define)
+		{
+			var targetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
+			var target = UnityEditor.Build.NamedBuildTarget.FromBuildTargetGroup(targetGroup);
+
+			var defineSet = new HashSet<string>(GetDefines(target));
+
+			if (defineSet.Remove(define))
+			{
+				SetDefines(target, defineSet);
+				Debug.Log("Removed scripting define: " + define);
+			}
+			else
+			{
+				Debug.Log("Scripting define not found: " + define);
+			}
+		}
+
+		private static void RemoveScriptingDefines(params string[] definesToRemove)
+		{
+			var targetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
+			var target = UnityEditor.Build.NamedBuildTarget.FromBuildTargetGroup(targetGroup);
+
+			var defineSet = new HashSet<string>(GetDefines(target));
+
+			bool changed = false;
+
+			foreach (var d in definesToRemove)
+			{
+				if (defineSet.Remove(d))
+					changed = true;
+			}
+
+			if (changed)
+			{
+				SetDefines(target, defineSet);
+				Debug.Log("Removed multiple scripting defines");
+			}
+		}
+
 		private static void OnCompilationFinished(object obj)
 		{
 			CompilationPipeline.compilationFinished -= OnCompilationFinished;
 			AddScriptingDefine("NEXTRIBE_FIREBASE");
+			AddScriptingDefine("NEXTRIBE_FB_ANALYTICS");
+			AddScriptingDefine("NEXTRIBE_FB_MESSAGING");
+			AddScriptingDefine("NEXTRIBE_FB_CRASHLYTICS");
+			AddScriptingDefine("NEXTRIBE_FB_REMOTE_CONFIG");
+		}
+
+		[MenuItem("Tools/Setup/Add/Firebase Storage")]
+		public static void AddFirebaseStorage()
+		{
+			AddScriptingDefine("NEXTRIBE_FB_FIRESTORE");
+		}
+		[MenuItem("Tools/Setup/Remove/Firebase Storage")]
+		public static void RemoveFirebaseStorage()
+		{
+			RemoveScriptingDefine("NEXTRIBE_FB_FIRESTORE");
+		}
+
+		[MenuItem("Tools/Setup/Add/Firebase Analytics")]
+		public static void AddFirebaseAnalytics()
+		{
+			AddScriptingDefine("NEXTRIBE_FB_ANALYTICS");
+		}
+		[MenuItem("Tools/Setup/Remove/Firebase Analytics")]
+		public static void RemoveFirebaseAnalytics()
+		{
+			RemoveScriptingDefine("NEXTRIBE_FB_ANALYTICS");
+		}
+
+		[MenuItem("Tools/Setup/Add/Firebase Messaging")]
+		public static void AddFirebaseMessaging()
+		{
+			AddScriptingDefine("NEXTRIBE_FB_MESSAGING");
+		}
+		[MenuItem("Tools/Setup/Remove/Firebase Messaging")]
+		public static void RemoveFirebaseMessaging()
+		{
+			RemoveScriptingDefine("NEXTRIBE_FB_MESSAGING");
+		}
+
+		[MenuItem("Tools/Setup/Add/Firebase Crashlytics")]
+		public static void AddFirebaseCrashlytics()
+		{
+			AddScriptingDefine("NEXTRIBE_FB_CRASHLYTICS");
+		}
+		[MenuItem("Tools/Setup/Remove/Firebase Crashlytics")]
+		public static void RemoveFirebaseCrashlytics()
+		{
+			RemoveScriptingDefine("NEXTRIBE_FB_CRASHLYTICS");
+		}
+
+		[MenuItem("Tools/Setup/Add/Firebase Firestore")]
+		public static void AddFirebaseFirestore()
+		{
+			AddScriptingDefine("NEXTRIBE_FB_FIRESTORE");
+		}
+		[MenuItem("Tools/Setup/Remove/Firebase Firestore")]
+		public static void RemoveFirebaseFirestore()
+		{
+			RemoveScriptingDefine("NEXTRIBE_FB_FIRESTORE");
+		}
+
+		[MenuItem("Tools/Setup/Add/Firebase Remote Config")]
+		public static void AddFirebaseRemoteConfig()
+		{
+			AddScriptingDefine("NEXTRIBE_FB_REMOTE_CONFIG");
+		}
+		[MenuItem("Tools/Setup/Remove/Firebase Remote Config")]
+		public static void RemoveFirebaseRemoteConfig()
+		{
+			RemoveScriptingDefine("NEXTRIBE_FB_REMOTE_CONFIG");
 		}
 
 		[MenuItem("NexTribe/Setup/Install Firebase SDK")]
